17489df1e6cbde7126ba86f5300956f868e60f06
wrocoder <polwroacc@gmail.com>
2024-04-03 10:25:42 +0200

    countries from db

---
+++ placetostay/common_scripts/tuple_countries.py
@@ -0,0 +1,50 @@
+def main():
+    countries_list = [
+        "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola",
+        "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Aruba", "Australia",
+        "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados",
+        "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
+        "Bonaire, Sint Eustatius and Saba", "Bosnia Herzegovina", "Botswana", "Brazil",
+        "British Virgin Islands", "Brunei Darussalam", "Bulgaria", "Burkina Faso",
+        "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands",
+        "Chad", "Chile", "China", "Colombia", "Comoros", "Cook Islands", "Costa Rica",
+        "Cote D'ivoire", "Croatia", "Curacao", "Cyprus", "Czech Republic",
+        "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica",
+        "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea",
+        "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Faroe Islands",
+        "Federated States of Micronesia", "Fiji", "Finland", "France", "French Guiana",
+        "French Polynesia", "Gabon", "Gambia", "Georgia", "Germany", "Ghana",
+        "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam",
+        "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guyana", "Haiti",
+        "Honduras", "Hong Kong SAR, China", "Hungary", "Iceland", "India", "Indonesia",
+        "Iraq", "Ireland", "Isle Of Man", "Israel", "Italy", "Jamaica", "Japan",
+        "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kosovo", "Kuwait", "Kyrgyzstan",
+        "Laos", "Latvia", "Lebanon", "Lesotho", "Liechtenstein", "Lithuania",
+        "Luxembourg", "Macau SAR, China", "Madagascar", "Malawi", "Malaysia",
+        "Maldives", "Mali", "Malta", "Martinique", "Mauritania", "Mauritius", "Mayotte",
+        "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat",
+        "Morocco", "Mozambique", "Myanmar", "Namibia", "Nepal", "Netherlands",
+        "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue",
+        "Norfolk Island", "North Macedonia", "Northern Mariana Islands", "Norway",
+        "Oman", "Pakistan", "Palau", "Palestinian Territory", "Panama", "Papua New Guinea",
+        "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico",
+        "Qatar", "Republic of Congo", "Reunion Island", "Romania", "Russia", "Rwanda",
+        "Saint Barthelemy", "Saint Kitts And Nevis", "Saint Lucia", "Saint Martin (France)",
+        "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal",
+        "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Sint Maarten (Netherlands)",
+        "Slovakia", "Slovenia", "Solomon Islands", "South Africa", "South Korea",
+        "South Sudan", "Spain", "Sri Lanka", "St. Vincent & Grenadines", "Suriname",
+        "Sweden", "Switzerland", "Taiwan", "Tajikistan", "Tanzania", "Thailand",
+        "Timor-Leste", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey",
+        "Turks & Caicos Islands", "U.S. Virgin Islands", "Uganda", "Ukraine",
+        "United Arab Emirates", "United Kingdom", "United States", "Uruguay",
+        "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Zambia", "Zimbabwe"
+    ]
+
+    countries_tuple_list = [(country, country) for country in countries_list]
+
+    print(countries_tuple_list)
+
+
+if __name__ == "__main__":
+    main()
